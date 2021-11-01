7dff09740281896c2783ebef6da3beb6e556a2b8
avvaru-venkatsai <venkatsai.avvaru93@gmail.com>
2021-11-01 12:21:12 +0100

    L3-end with amazon Link

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             // solution
-            url: 'https://www.vuemastery.com/'
+            url: 'https://www.amazon.it/X-Socks-Trek-Cotton-Socks-Unisex/dp/B07PYSLK14/ref=sr_1_12?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=green+socks&qid=1635764417&qsid=260-0529218-1280312&sr=8-12&sres=B08R7FHND9%2CB08MY9BS67%2CB07HBC5YR9%2CB08MY5CZR1%2CB08MY2JRXZ%2CB085J9NNZG%2CB07QYGRJB3%2CB07PYSLK14%2CB07N892FKJ%2CB07N8S5RZY%2CB00LW4MVO6%2CB07FFK39JP%2CB07W5JVNFJ%2CB08P51XPK7%2CB07Y29YYXC%2CB08KWDM26S%2CB08MY8M2PD%2CB08ZXPX8FB%2CB08DL7Y8F5%2CB089QQB5H5&srpt=SOCKS'
             // solution
         }
     }
