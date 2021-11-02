b07a60ec57e4f2d7bad34011cfd2aead26c9b589
avvaru-venkatsai <venkatsai.avvaru93@gmail.com>
2021-11-02 17:04:26 +0100

    Components & Props revised

--- assets/components/productDetails.js
+++ assets/components/productDetails.js
@@ -1,16 +1,14 @@
 app.component("product-details", {
     props: {
         details: {
-            type: list,
+            type: Array,
             required:Boolean
         }
     },
 
     template:
-    `<div class="product-info">
-        <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-    </div>`,
+    `<ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>`,
     
 })
--- assets/components/productDisplay.js
+++ assets/components/productDisplay.js
@@ -18,6 +18,7 @@ app.component("product-display", {
             <p v-if="inStock">In Stock</p>
             <p v-else>Out of Stock</p>
             <p> Shipping: {{ shipping }} </p>
+            <product-details :details="details"></product-details>
             <div 
               v-for="(variant, index) in variants" 
               :key="variant.id" 
@@ -36,6 +37,7 @@ app.component("product-display", {
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
